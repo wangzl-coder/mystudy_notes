6c5435488ed433e28d1bf0ca5f52286d97418786
wangzl <17809285424@163.com>
2022-12-13 13:11:23 +0000

    [feat] add documentation

--- linux_c/sec10_pthread/demo_hash_table.c
+++ linux_c/sec10_pthread/demo_hash_table.c
@@ -97,3 +97,76 @@ struct void foo_rele(struct foo *fp)
         pthread_mutex_unlock(&fp->f_mutex);
     }
 }
+
+/* 优化，可用散列表锁来保护数据引用结构，简化代码 */
+
+struct foo *foo_alloc(int id)
+{
+    struct foo *fp;
+    if((fp = malloc(sizeof(struct foo))) == NULL) {
+        fprintf(stderr, "Error: failed to malloc for struct foo! \r\n");
+        return NULL;
+    }
+    fp->count = 1;
+    fp->id = id;
+    if(pthread_mutex_init(&fp->f_mutex)){
+        fprintf(stderr, "Error: failed to init fp->f_mutex \r\n");
+        free(fp);
+        return NULL;
+    }
+    pthread_mutex_lock(&hashlock);
+    fp->f_next = ftable[hash(id)];
+    ftable[hash(id)] = fp;
+    pthread_mutex_lock(&fp->f_mutex);
+    pthread_mutex_unlock(&hashlock);
+    /* continue initialization */
+    pthread_mutex_unlock(&fp->f_mutex);
+    return fp;
+}
+
+void foo_hold(struct foo *fp)
+{
+    pthread_mutex_lock(&hashlock);
+    fp->count ++;
+    pthread_mutex_unlock(&hashlock);
+}
+
+struct foo *foo_find(int id)
+{
+    struct foo *fp;
+
+    pthread_mutex_lock(&hashlock);
+    for(fp = ftable[hash(id)]; fp != NULL; fp = fp->next) {
+        if(fp->id == id)
+            break;
+    }
+    pthread_mutex_unlock(&hashlock);
+    return fp;
+}
+
+void foo_rele(struct foo *fp)
+{
+    struct foo *tfp;
+    int index;
+
+    pthread_mutex_lock(&hashlock);
+    if(--fp->count == 0) {          /* last refrence */
+        index = hash(id);
+        tfp = ftable[index];
+        if(tfp == fp) {
+            ftable[index] = fp->next;
+        } else {
+            while(tfp->next != fp) {
+                tfp = tfp->next;
+            }
+            tfp->next = fp->next;
+        }
+        pthread_mutex_unlock(&hashlock);
+        pthread_mutex_destroy(fp->f_mutex);
+        free(fp);
+    } else {
+        pthread_mutex_unlock(&hashlock);
+    }
+}
+
+
