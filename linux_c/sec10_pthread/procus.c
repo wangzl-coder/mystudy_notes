6fab4bcb8a56bc086b9eb37c1e352e3fc8971fd6
wangzl-coder <17809285424@163.com>
2022-12-15 16:39:18 +0800

    [feat] add for prodoctor and customer model by semaphore

--- linux_c/sec10_pthread/procus.c
+++ linux_c/sec10_pthread/procus.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-
+#include <errno.h>
+#include <string.h>
+#include <semaphore.h>
 
 #define ITEM_OFFSET(type, itname)  (size_t)(&(((type*)0)->itname))
-
 #define ITEM_ENTRY(type, pitem, itname) ((type*) (((char *) pitem) - ITEM_OFFSET(type, itname)))
 
-struct list_st{
+#define PRODUCT_MAX     (20)
+#define PRODUCTOR_NUM   (3)
+#define CUSTOMER_NUM    (10)
+
+typedef struct list_st{
     struct list_st *next;
     struct list_st *prev;
 }list;
 
-struct product_item_st{
-    char *name;
-    int num;
-};
-
 struct queue_st{
     list qhead;
-    list *qtail;
+    size_t size;
 };
 
 typedef struct queue_item_st{
@@ -27,36 +26,40 @@ typedef struct queue_item_st{
     void *data;
 }queue_item;
 
-static int queue_init(struct queue_st *queue)
+struct product_param_st{
+    struct queue_st *queue;
+    sem_t profull;
+    sem_t proempty;
+    pthread_mutex_t lock;
+};
+
+struct queue_st *queue_init(size_t datalen)
 {
+    struct queue_st *queue;
+    queue = malloc(sizeof(struct queue_st));
     if(queue == NULL)
-        return -EINVAL;
-    queue->qhead.next = NULL;
-    queue->qhead.prev = NULL;
-    queue->qtail = &queue->qhead;
-    return 0;
+        return NULL;
+    queue->qhead.next = &queue->qhead;
+    queue->qhead.prev = &queue->qhead;
+    queue->size = datalen;
+    return queue;
 }
 
 /* add tail */
-static int queue_in(struct queue_st *queue, void *data, unsigned int size)
+int queue_in(struct queue_st *queue, void *data)
 {
     queue_item *item;
     if(queue == NULL)
-        rerturn -EINVAL;
+        return -EINVAL;
     if( (item = malloc(sizeof(queue_item))) == NULL)
         return -ENOMEM;
-    if((newdata = malloc(size)) == NULL)
+    if((item->data = malloc(queue->size)) == NULL)
         goto item_free;
-    memcpy(item->data, data, size);
-    item->node.next = NULL;
-    if(queue->qtail == &queue->head) {      /* null node yet */
-        queue->head.next = &item->node;
-        item->node.prev = &queue->head;
-    } else {
-        queue->qtail->next = &item->node;
-        item->node.prev = queue->tail;
-    }
-    queue->qtail = &item->node;
+    memcpy(item->data, data, queue->size);
+    item->node.next = &queue->qhead;
+    item->node.prev = queue->qhead.prev;
+    queue->qhead.prev->next = &item->node;
+    queue->qhead.prev = &item->node;
     return 0;
 
 item_free:
@@ -65,36 +68,100 @@ item_free:
 }
 
 /* del head */
-static void *queue_out(struct queue_st *queue)
+int queue_out(struct queue_st *queue, void *data)
 {
-    list *del_node = queue->qtail;
-    queue_item *del_item;
-    if(del_node == &queue->head)    /* null node yet */
-        return NULL;
-    
-    del_item = ITEM_ENTRY(queue_item, del_node, node);
-    queue->qtail = del_node->prev;
-    queue->qtail->next = NULL;
-    free();
-    free(del_item);
+    list *del_node = queue->qhead.next;
+    queue_item *item;
+    if(del_node == &queue->qhead)
+        return 0;
+    item = ITEM_ENTRY(queue_item, del_node, node);
+    memcpy(data, item->data, queue->size);
+    del_node->next->prev = del_node->prev;
+    del_node->prev->next = del_node->next;
+    free(item->data);
+    free(item);
+    return 0;
 }
 
-static void *producer(void *arg)
+void queue_destroy(struct queue_st *queue)
 {
-    struct product_st *product = arg;
-          
-    
+    list *item_node, *tmp;
+    queue_item *item;
+    int i = 0;
+    for(item_node = queue->qhead.next; item_node != &queue->qhead; item_node = tmp) {
+        item = ITEM_ENTRY(queue_item, item_node, node);
+        tmp = item_node->next;
+        free(item->data);
+        free(item);
+    }
+    free(queue);
 }
 
-static void *customer(void *arg)
+static int senval;
+
+void *productor_process(void *arg)
 {
+    struct product_param_st *param = arg;
+    while(1) {
+        sleep(1);
+        sem_wait(&param->profull);
+        pthread_mutex_lock(&param->lock);
+        senval ++;
+        printf("put value %d \r\n", senval);
+        queue_in(param->queue, &senval);
+        pthread_mutex_unlock(&param->lock);
+        sem_post(&param->proempty);
+    }
+    return (void *)0;
+}
 
-
+void *customer_process(void *arg)
+{
+    struct product_param_st *param = arg;
+    while(1) {
+        int recval;
+        sleep(5);
+        sem_wait(&param->proempty);
+        pthread_mutex_lock(&param->lock);
+        queue_out(param->queue, &recval);
+        printf("catch number %d \r\n", recval);
+        pthread_mutex_unlock(&param->lock);
+        sem_post(&param->profull);
+    }
+    return (void *)0;
 }
 
 int main()
 {
-    
+    int i;
+    int err;
+    pthread_t ptid[PRODUCTOR_NUM], ctid[CUSTOMER_NUM];
+    struct product_param_st param;
+    if ((err = pthread_mutex_init(&param.lock, NULL))) {
+        fprintf(stderr, "wanrnning : failed to init lock : %s \r\n", strerror(err));
+        exit(-1);
+    }
+    pthread_mutex_init(&param.lock, NULL);
+    sem_init(&param.profull, 0, PRODUCT_MAX);
+    sem_init(&param.proempty, 0, 0);
+    param.queue = queue_init(sizeof(int));
+    for(i = 0; i < PRODUCTOR_NUM; i++) {
+        printf("pro: %d \r\n", i);
+        pthread_create(&ptid[i], NULL, productor_process, &param);
+    }
+    for(i = 0; i < CUSTOMER_NUM; i++) {
+        pthread_create(&ctid[i], NULL, customer_process, &param);
+    }
 
+    for(i = 0; i < PRODUCTOR_NUM; i++) {
+        pthread_join(&ptid[i], NULL);
+    }
+    for(i = 0; i < CUSTOMER_NUM; i++) {
+        pthread_join(&ctid[i], NULL);
+    }
+    pthread_mutex_destroy(&param.lock);
+    sem_destroy(&param.profull);
+    sem_destroy(&param.proempty);
+    queue_destroy(param.queue);
     exit(0);
 }
